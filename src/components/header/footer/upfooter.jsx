fbbfbc7e5b688f29f8d0941c05a311366d5d37e4
eslam <ealaa3764@gmail.com>
2024-02-29 02:51:04 +0200

    facebook

--- src/components/header/footer/upfooter.jsx
+++ src/components/header/footer/upfooter.jsx
@@ -6,14 +6,14 @@ const UpFooter = () => {
       <h4>متواجدين دائما لمساعدتك</h4>
 
        <div className="icons">
-       <div><a href="#"> <i class="bi bi-facebook face"></i>  </a></div>
+       <div><a href="https://web.facebook.com/profile.php?id=61556565194892"> <i class="bi bi-facebook face"></i>  </a></div>
        <div><a href="#"> <i class="bi bi-instagram insta"></i> </a></div>
        <div><a href="#"> <i class="bi bi-whatsapp whats"></i> </a></div>
        </div>
 
        <div className="contactUs">
         <h4>تواصل معنا عبر الايميل</h4>
-        <h3><i class="bi bi-envelope"></i>Waseet@gmail.com</h3>
+        <a href="mailto:waseet288@gmail.com"><h3><i class="bi bi-envelope"></i>Waseet</h3></a>
        </div>
        
     </div>
